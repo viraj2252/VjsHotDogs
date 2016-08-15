e112d728b4f44ad8b963c2b0652822694fc40cc7
viraj2252 <viraj2252@gmail.com>
2016-08-15 18:11:09 +0800

    Add project files.

---
+++ VjsHotDogs.Core/Model/HotDogGroup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VjsHotDogs.Core.Model
+{
+    public class HotDogGroup
+    {
+        public HotDogGroup()
+        {
+        }
+
+        public int HotDogGroupId
+        {
+            get;
+            set;
+        }
+
+        public string Title
+        {
+            get;
+            set;
+        }
+
+        public string ImagePath
+        {
+            get;
+            set;
+        }
+
+        public List<HotDog> HotDogs
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ VjsHotDogs.Core/Service/HotDogDataService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VjsHotDogs.Core.Model;
+using VjsHotDogs.Core.Repository;
+
+namespace VjsHotDogs.Core.Service
+{
+    public class HotDogDataService
+    {
+        //private static HotDogWebRepository hotDogRepository = new HotDogWebRepository();
+        private static HotDogRepository hotDogRepository = new HotDogRepository();
+
+        public HotDogDataService()
+        {
+        }
+
+        public List<HotDog> GetAllHotDogs()
+        {
+            return hotDogRepository.GetAllHotDogs();
+        }
+
+        public List<HotDogGroup> GetGroupedHotDogs()
+        {
+            return hotDogRepository.GetGroupedHotDogs();
+        }
+
+        public List<HotDog> GetHotDogsForGroup(int hotDogGroupId)
+        {
+            return hotDogRepository.GetHotDogsForGroup(hotDogGroupId);
+        }
+
+        public List<HotDog> GetFavoriteHotDogs()
+        {
+            return hotDogRepository.GetFavoriteHotDogs();
+        }
+
+        public HotDog GetHotDogById(int hotDogId)
+        {
+            return hotDogRepository.GetHotDogById(hotDogId);
+        }
+
+    }
+}
